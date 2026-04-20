4dc2289579a68337285f49d1252a672341665389
almaslof <187766901+almaslof@users.noreply.github.com>
2026-04-20 12:56:50 +0300

    ttt

--- tutorials_python/ttt.py
+++ tutorials_python/ttt.py
@@ -18,11 +18,18 @@ def add_kernel(x_ptr, y_ptr, output_ptr, n_elements, BLOCK_SIZE: tl.constexpr,):
     #output = x + y
     #tl.store(output_ptr + offsets, output, mask=mask)
 
+def add(x: torch.Tensor, y: torch.Tensor):
+    output = torch.empty_like(x)
+    assert x.device == DEVICE and y.device == DEVICE and output.device == DEVICE
+    n_elements = output.numel()
+    grid = lambda meta: (triton.cdiv(n_elements, meta['BLOCK_SIZE']), )
+    add_kernel[grid](x, y, output, n_elements, BLOCK_SIZE=1024)
+    return output
+
 torch.manual_seed(0)
 size = 98432
 x = torch.rand(size, device=DEVICE)
 y = torch.rand(size, device=DEVICE)
 output_torch = x + y
-#output_triton = add(x, y)
-add_kernel()
+output_triton = add(x, y)
 
